2e801a1fd1b0ecad6f5b7b40f3fd499440008de4
Areth Kellejian <arethk@gmail.com>
2026-09-15 11:45:27 -0700

    added hello wold endpoint

---
+++ src/main/java/com/areth/springboot/todowebapp/hello/SayHelloController.java
@@ -0,0 +1,15 @@
+package com.areth.springboot.todowebapp.hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class SayHelloController {
+
+	@RequestMapping("hello")
+	@ResponseBody
+	public String sayHello() {
+		return "Hello World!";
+	}
+}
